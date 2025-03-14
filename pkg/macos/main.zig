421b72d57d95fb9548a3f8a34699e5c18606b086
Mitchell Hashimoto <m@mitchellh.com>
2025-03-14 14:43:45 -0700

    pkg/macos: clean up for Zig 0.14, consolidate C imports into one decl
    Fixes #6727
    
    The major change in this commit is to consolidate all the C imports in
    a single decl in main.zig. This is required for Zig 0.14. Without it,
    the problem in #6727 will happen. I was never able to minimize why this
    happens in order to open a Zig bug.
    
    Beyond this, I fixed the build.zig and build.zig.zon to work with Zig
    0.14 so that we can test building `pkg/macos` in isolation. There are no
    downstream impacting changes in the build.zig files.

--- pkg/macos/main.zig
+++ pkg/macos/main.zig
@@ -7,6 +7,20 @@ pub const os = @import("os.zig");
 pub const text = @import("text.zig");
 pub const video = @import("video.zig");
 
+// All of our C imports consolidated into one place. We used to
+// import them one by one in each package but Zig 0.14 has some
+// kind of issue with that I wasn't able to minimize.
+pub const c = @cImport({
+    @cInclude("Carbon/Carbon.h");
+    @cInclude("CoreFoundation/CoreFoundation.h");
+    @cInclude("CoreGraphics/CoreGraphics.h");
+    @cInclude("CoreText/CoreText.h");
+    @cInclude("CoreVideo/CoreVideo.h");
+    @cInclude("QuartzCore/CALayer.h");
+    @cInclude("dispatch/dispatch.h");
+    @cInclude("os/log.h");
+});
+
 test {
     @import("std").testing.refAllDecls(@This());
 }
